a237540379bcae52ff2d9c7d997ff76eba365bca
Urjeet Patel <violentmagician.opensource@gmail.com>
2019-09-11 23:26:12 -0500

    Add chat app

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,5 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
 
-urlpatterns = [path("admin/", admin.site.urls)]
+urlpatterns = [path("admin/", admin.site.urls), path("chat/", include("chat.urls"))]
