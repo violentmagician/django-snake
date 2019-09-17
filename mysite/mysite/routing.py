e18e6e0a585cb437ccab37c2992e6fd89b960383
Urjeet Patel <violentmagician.opensource@gmail.com>
2019-09-17 18:39:49 -0500

    Sync Consumer

--- mysite/mysite/routing.py
+++ mysite/mysite/routing.py
@@ -1,7 +1,10 @@
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import chat.routing
 
 application = ProtocolTypeRouter(
     {
         # (http->django views is added by default)
+        "websocket": AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))
     }
 )
